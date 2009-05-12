da7788f0568e53bfa281cf93965ee2850289f3e4
Paul Howe <pahowe@gmail.com>
2009-05-12 14:39:41 -0400

    more stuff for /login (routing)

--- urls.py
+++ urls.py
@@ -12,6 +12,7 @@ urlpatterns = patterns('',
     (r'^set_basedir$','p3.filesync.views.set_basedir'),
     (r'^download(?P<filepath>.*)','p3.filesync.views.download'),
     (r'^upload_to_gdocs(?P<filepath>.*)','p3.filesync.views.upload_to_gdocs'),
+    (r'^login$','p3.filesync.views.login'),
 
     # If this gets too full of p3 stuff, can re-factor like so: 
     # (r'^p3/', include('p3.foo.urls')),
