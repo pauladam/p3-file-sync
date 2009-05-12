e7fd3fbaf73a13b7f0e6d0120de1673b48ed0073
Paul Howe <pahowe@gmail.com>
2009-05-12 03:19:01 -0400

    enabling django admin

--- urls.py
+++ urls.py
@@ -1,8 +1,11 @@
 from django.conf.urls.defaults import *
 
 # Uncomment the next two lines to enable the admin:
-# from django.contrib import admin
-# admin.autodiscover()
+from django.contrib import admin
+admin.autodiscover()
+
+import os
+HOME = os.getcwd()
 
 urlpatterns = patterns('',
     # Example:
@@ -13,5 +16,5 @@ urlpatterns = patterns('',
     # (r'^admin/doc/', include('django.contrib.admindocs.urls')),
 
     # Uncomment the next line to enable the admin:
-    # (r'^admin/(.*)', admin.site.root),
+    (r'^admin/(.*)', admin.site.root),
 )
