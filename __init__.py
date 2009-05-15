a37d5f841a3c1b8df9e905cfb9f80a10308e58fa
Paul Howe <pahowe@gmail.com>
2009-05-15 06:05:37 -0400

    adding a hook on startup to start the fs watcher (since it typically only gets run on a directory change)
    on 2nd thought we probably want to set the rootdir/device name
    as a persistant data tuplet for this running server (host:port combo)
    in the db. and then check that from the db on startup since this will
    also tell us what values to use for devicename/rootdir

---
+++ __init__.py
@@ -0,0 +1,38 @@
+# Filesync app start up code here:
+from filesync.models import File
+import threading, sys, os
+from background import check_fs
+
+# Create a token so we can know if weve 'run' already
+# since django executes this module multiple times.
+# A less bad approach would be to detect in python 
+# if our thread is running already, we could do that
+# but this might suffice
+FS_SCAN_LOCK = '/tmp/filesync_scan_fs_start_lock'
+
+try:
+  statinfo = os.stat(FS_SCAN_LOCK)
+
+  # Otherwise, this state is locked, a thread is running so just bail
+  if statinfo:
+    thread_running = True
+except OSError:
+  # No threads started yet, create a lock
+  file(FS_SCAN_LOCK,'w').write('ok')
+  thread_running = False
+
+print sys._getframe(1).f_code.co_name
+
+# Shorthand for, are we configured and 
+# prepared to start watching the fs?
+if not thread_running and len(File.objects.all()) > 0:
+  # Error prone when we have multiple roots, devices
+  # we will always only have one local device but we may support
+  # remote devices in the future... so basically we need to bind 
+  # the (device name, root dir) tuple into the server instance
+  # then wed use that tuple here. until then just use the first couplet we find...
+  f = File.objects.all()[:1].get()
+  basedir, device_name = f.rootdir, f.device_name 
+
+  print 'starting thread'
+  t = threading.Timer(0.0, check_fs, kwargs={'root':basedir, 'device_name':device_name}).start()
