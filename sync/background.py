96ac37a306c96a2f7e71803478e8fa211814d165
Paul Howe <pahowe@gmail.com>
2009-05-17 03:35:50 -0400

    making into a class and referring to member properties

--- sync/background.py
+++ sync/background.py
@@ -53,6 +53,59 @@ class FSChecker:
                  rootdir=self.root,
                  device=this_device).save()
 
-    # And reset timer
-    lt = threading.Timer(5.0, check_fs, kwargs={'root':root,'device_name':device_name}).start()
-    
+      # Run 1 + n
+      # Check if any updates are needed to the files in our db
+      for file in File.objects.filter(deleted=False, device=this_device):
+        try:
+          statinfo = os.stat(file.full_path)
+            
+          # sizes differ, adjust size
+          if file.size != statinfo.st_size:
+            debug('adjusting file sizes: %s ' % file)
+            file.size = statinfo.st_size
+            file.save()
+
+          # mtimes differ, adjust mtime
+          if file.mtime != statinfo.st_mtime:
+            debug('adjusting file mtime: %s ' % file)
+            file.mtime = statinfo.st_mtime
+            file.save()
+
+        except OSError, oserror:
+          if oserror.strerror.startswith('No such file'):
+            # The file was deleted
+            debug('changing file deleted status false->true: %s' % file  )
+            file.deleted = True
+            file.save()
+          
+      # Do a fs walk just to check for new files
+      known_files = [file.full_path for file in File.objects.all()]
+      for path, dirs, files in os.walk(self.root):
+          for f in files:
+            # Either a) we know about the file (boring)
+            #     or b) its been undeleted (set deleted flag from true to false)
+            #     or c) its new and needs to be added (make a new file object and save it)
+            full_file_path = "%s/%s" % (path,f)
+
+            if full_file_path in known_files:
+              file_from_db = File.objects.filter(full_path=full_file_path)[:1].get() # using .get() like .one()
+              # Set deleted flag to false
+              if file_from_db.deleted == True:
+                debug('changing file deleted status true->false: %s' % file_from_db)
+                file_from_db.deleted = False
+                file_from_db.save()
+
+            # Totally new file, add it
+            elif full_file_path not in known_files:
+              statinfo = os.stat(full_file_path)
+              debug('[%s] found a new file, adding it: %s' % (this_device, full_file_path))
+              File(name=f, 
+                   size=statinfo.st_size, 
+                   mtime=statinfo.st_mtime, 
+                   path=path, 
+                   full_path=full_file_path, 
+                   device_name=self.device_name,
+                   rootdir=self.root,
+                   device=this_device).save()
+
+
