49aa030c378cdd27278995b4acccfba1b78e511c
Paul Howe <pahowe@gmail.com>
2009-05-17 03:31:45 -0400

    made check_fs intoa  class which runs a while true loop but its spawned via thread so , non-blocking AND only one long living thread is needeed

--- fns/fns_utils.py
+++ fns/fns_utils.py
@@ -3,20 +3,22 @@ import urllib
 
 def announce_hostlist():
 
-  print 'announcing hostlist'
-
   host_set = set([h.hostname for h in Host.objects.all()])
 
   for h in host_set:
     ann_list = ','.join(list(host_set.difference(set([h]))))
-    url = '/'.join(['http:/',h,'acceptpeerlist',ann_list])
-    print 'Telling %s about peers: %s [%s]' % (h, ann_list, url)
-    try:
-      urllib.urlopen(url).read()
-    except:
-      # Ok this is kind of dangerous in that we are
-      # catching all exceptions, but I think sometimes
-      # we are announcing to a dead peer in which this 
-      # should throw some type of socket open exception
-      pass
+
+    # In case there is someone to notify
+    if ann_list:
+      url = '/'.join(['http:/',h,'acceptpeerlist',ann_list])
+      print 'Announce list for %s : %s' % (h, ann_list)
+      print 'Telling %s about peers: %s [%s]' % (h, ann_list, url)
+      try:
+        urllib.urlopen(url).read()
+      except:
+        # Ok this is kind of dangerous in that we are
+        # catching all exceptions, but I think sometimes
+        # we are announcing to a dead peer in which this 
+        # should throw some type of socket open exception
+        pass
  
--- sync/background.py
+++ sync/background.py
@@ -1,94 +1,56 @@
 import time, os, re, sys, threading
 from filesync.models import File, Device
 import common_utils
+import sched, time
+from utils import debug
 
 # TODO: Check fs isnt started on server start, only after a
 # dir is set. we should start it if we can and we have files to watch..
 
-if 'runserver' in sys.argv:
-  this_device = Device.objects.filter(hnportcombo=common_utils.get_hostname()).get()
+class FSChecker:
 
-  def check_fs(root, device_name):
-    # Run 1
-    if len(File.objects.all()) < 1:
-      print 'first run'
+  def __init__(self, root, device_name):
+    self.root = root
+    self.device_name = device_name
+    self.running = False
 
-      for path, dirs, files in os.walk(root):
-        for f in files:
-          print path, f
-          full_file_path = '/'.join([path,f])
+  def start(self):
 
-          print full_file_path
+    if self.running:
+      return
+    self.running = True
 
-          mtime = os.stat(full_file_path).st_mtime
-          size = os.stat(full_file_path).st_size
-          # Terse I know :) but its fine, seriously dont worry about it 
-          File(name=f, 
-               size=size, 
-               mtime=mtime, 
-               path=path, 
-               full_path=full_file_path, 
-               device_name=device_name,
-               rootdir=root,
-               device=this_device).save()
+    t = threading.Timer(0.0, self.check_fs, kwargs={}).start()
 
-    # Run 1 + n
-    # sys.stderr.write('.')
-    time.sleep(2)
+  def check_fs(self):
 
-    # Check if any updates are needed to the files in our db
-    for file in File.objects.filter(deleted=False):
-      try:
-        statinfo = os.stat(file.full_path)
-          
-        # sizes differ, adjust size
-        if file.size != statinfo.st_size:
-          print 'adjusting file sizes: %s ' % file
-          file.size = statinfo.st_size
-          file.save()
+    #if 'runserver' in sys.argv:
+    #  this_device = Device.objects.filter(hnportcombo=common_utils.get_hostname()).get()
 
-        # mtimes differ, adjust mtime
-        if file.mtime != statinfo.st_mtime:
-          print 'adjusting file mtime: %s ' % file
-          file.mtime = statinfo.st_mtime
-          file.save()
+    # TODO: while not stopped
+    while True:
+      time.sleep(2)
+    
+      this_device = Device.objects.filter(hnportcombo=common_utils.get_hostname()).get()
 
-      except OSError, oserror:
-        if oserror.strerror.startswith('No such file'):
-          # The file was deleted
-          print 'changing file deleted status false->true: %s' % file  
-          file.deleted = True
-          file.save()
-        
-    # Do a fs walk just to check for new files
-    known_files = [file.full_path for file in File.objects.all()]
-    for path, dirs, files in os.walk(root):
-      if len(dirs) == 0:
-        for f in files:
-          # Either a) we know about the file (boring)
-          #     or b) its been undeleted (set deleted flag from true to false)
-          #     or c) its new and needs to be added (make a new file object and save it)
-          full_file_path = "%s/%s" % (path,f)
+      # Run 1
+      if len(File.objects.filter(device=this_device)) < 1:
+        debug('first run')
 
-          if full_file_path in known_files:
-            file_from_db = File.objects.filter(full_path=full_file_path)[:1].get() # using .get() like .one()
-            # Set deleted flag to false
-            if file_from_db.deleted == True:
-              print 'changing file deleted status true->false: %s' % file_from_db
-              file_from_db.deleted = False
-              file_from_db.save()
+        for path, dirs, files in os.walk(self.root):
+          for f in files:
+            full_file_path = '/'.join([path,f])
 
-          # Totally new file, add it
-          elif full_file_path not in known_files:
-            statinfo = os.stat(full_file_path)
-            print 'found a new file, adding it: %s' % full_file_path
+            mtime = os.stat(full_file_path).st_mtime
+            size = os.stat(full_file_path).st_size
+            # Terse I know :) but its fine, seriously dont worry about it 
             File(name=f, 
-                 size=statinfo.st_size, 
-                 mtime=statinfo.st_mtime, 
+                 size=size, 
+                 mtime=mtime, 
                  path=path, 
                  full_path=full_file_path, 
-                 device_name=device_name,
-                 rootdir=root,
+                 device_name=self.device_name,
+                 rootdir=self.root,
                  device=this_device).save()
 
     # And reset timer
